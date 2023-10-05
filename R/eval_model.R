747908ecf994cd6b8b3c79a1e3a9f4fe74f25819
manuel-kunz <kunzmanuel@outlook.com>
2023-10-05 09:42:51 +0200

    chapter 10

--- R/eval_model.R
+++ R/eval_model.R
@@ -1,58 +1,76 @@
-# make model evaluation into a function to reuse code
-eval_model <- function(mod, df_train, df_test){
+#' Evaluate model performance
+#'
+#' @param mod the model formulation
+#' @param df_train the training data
+#' @param df_test the testing data
+#' @param return_metrics the metrics / figures returned
+#'
+#' @return model metrics
+
+eval_model <- function(mod, df_train, df_test, return_metrics = FALSE){
   
-  # add predictions to the data frames
-  df_train <- df_train |> 
-    drop_na()
-  df_train$fitted <- predict(mod, newdata = df_train)
+  require(magrittr)  # part of dplyr, required for the "old" pipe
   
-  df_test <- df_test |> 
-    drop_na()
-  df_test$fitted <- predict(mod, newdata = df_test)
+  # add predictions to the data frames----
+  df_train <- df_train %>% 
+    drop_na() %>%  # magrittr pipe necessary here for the dot ('.') evaluation
+    mutate(fitted =  predict(mod, newdata = .))
   
-  # get metrics tables
-  metrics_train <- df_train |> 
+  df_test <- df_test %>% 
+    drop_na() %>% 
+    mutate(fitted =  predict(mod, newdata = .))
+  
+  # get metrics tables----
+  metrics_train <- df_train %>% 
     yardstick::metrics(GPP_NT_VUT_REF, fitted)
   
-  metrics_test <- df_test |> 
+  metrics_test <- df_test %>% 
     yardstick::metrics(GPP_NT_VUT_REF, fitted)
   
-  # extract values from metrics tables
-  rmse_train <- metrics_train |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_train <- metrics_train |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  rmse_test <- metrics_test |> 
-    filter(.metric == "rmse") |> 
-    pull(.estimate)
-  rsq_test <- metrics_test |> 
-    filter(.metric == "rsq") |> 
-    pull(.estimate)
-  
-  # visualise as a scatterplot
-  # adding information of metrics as sub-titles
-  plot_1 <- ggplot(data = df_train, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_train, digits = 2)) ~~
-                              RMSE == .(format(rmse_train, digits = 3))),
-         title = "Training set") +
-    theme_classic()
-  
-  plot_2 <- ggplot(data = df_test, aes(GPP_NT_VUT_REF, fitted)) +
-    geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
-    geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
-    labs(subtitle = bquote( italic(R)^2 == .(format(rsq_test, digits = 2)) ~~
-                              RMSE == .(format(rmse_test, digits = 3))),
-         title = "Test set") +
-    theme_classic()
-  
-  out <- cowplot::plot_grid(plot_1, plot_2)
-  
-  return(out)
+  if (return_metrics){
+    
+    return(list(train = metrics_train, test = metrics_test))
+    
+  } else {
+    # extract values from metrics tables----
+    rmse_train <- metrics_train %>% 
+      filter(.metric == "rmse") %>% 
+      pull(.estimate)
+    rsq_train <- metrics_train %>% 
+      filter(.metric == "rsq") %>% 
+      pull(.estimate)
+    
+    rmse_test <- metrics_test %>% 
+      filter(.metric == "rmse") %>% 
+      pull(.estimate)
+    rsq_test <- metrics_test %>% 
+      filter(.metric == "rsq") %>% 
+      pull(.estimate)
+    
+    # visualise as a scatterplot----
+    # adding information of metrics as sub-titles
+    gg1 <- df_train %>% 
+      ggplot(aes(GPP_NT_VUT_REF, fitted)) +
+      geom_point(alpha = 0.3) +
+      geom_smooth(method = "lm", se = FALSE, color = "red") +
+      geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
+      labs(subtitle = bquote( italic(R)^2 == .(format(rsq_train, digits = 2)) ~~
+                                RMSE == .(format(rmse_train, digits = 3))),
+           title = "Training set") +
+      theme_classic()
+    
+    gg2 <- df_test %>% 
+      ggplot(aes(GPP_NT_VUT_REF, fitted)) +
+      geom_point(alpha = 0.3) +
+      geom_smooth(method = "lm", se = FALSE, color = "red") +
+      geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
+      labs(subtitle = bquote( italic(R)^2 == .(format(rsq_test, digits = 2)) ~~
+                                RMSE == .(format(rmse_test, digits = 3))),
+           title = "Test set") +
+      theme_classic()
+    
+    out <- cowplot::plot_grid(gg1, gg2)
+    
+    return(out) 
+  }
 }
